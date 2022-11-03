18ab278e20f5ac8a2c06e237c24a7de0aeea9c0b
Marleess Windows <marleysumarly@gmail.com>
2022-11-04 04:55:50 +0700

    Resum pake array, ngeditnya di load.php

---
+++ load.php
@@ -0,0 +1,23 @@
+<?php
+
+$model = [
+    0 => [
+        'nama' => 'Sumarli',
+        'photo' => 'assets/img/photos/marleess.jpg',
+        'nim' => '21416255201201',
+        'kelas' => 'IF21F',
+    ],
+    1 => [
+        'nama' => 'Muhammad Fauzan Zakian',
+        'photo' => 'assets/img/photos/fauzan.jpg',
+        'nim' => '21416255',
+        'kelas' => 'IF21F',
+    ],
+    2 => [
+        'nama' => 'Fuad Adhitya Warman',
+        'photo' => 'assets/img/photos/fuad.jpg',
+        'nim' => '21416255xx',
+        'kelas' => 'IF21F',
+    ],
+
+];
