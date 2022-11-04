a0de0e6417847239063b6abb8b6fc191f2f4b42c
AldimasLaksono <aldimaslaksono.stemba@gmail.com>
2022-11-04 19:12:09 +0700

    Upload Foto

--- load.php
+++ load.php
@@ -10,13 +10,49 @@ $model = [
     1 => [
         'nama' => 'Muhammad Fauzan Zakian',
         'photo' => 'assets/img/photos/fauzan.jpg',
-        'nim' => '21416255201112',
+        'nim' => '21416255',
         'kelas' => 'IF21F',
     ],
     2 => [
         'nama' => 'Fuad Adhitya Warman',
         'photo' => 'assets/img/photos/fuad.jpg',
         'nim' => '21416255xx',
+        'kelas' => 'IF21F',
+    ],
+    3 => [
+        'nama' => 'Aldimas Laksono',
+        'photo' => 'assets/img/photos/Dimas.JPG',
+        'nim' => '21416255201050',
+        'kelas' => 'IF21F',
+    ],
+    4 => [
+        'nama' => 'Risma Marina',
+        'photo' => 'assets/img/photos/Risma.jpg',
+        'nim' => '21416255201039',
+        'kelas' => 'IF21F',
+    ],
+    5 => [
+        'nama' => 'Septinas Marzuq Hadi Putra',
+        'photo' => 'assets/img/photos/Tian.jpg',
+        'nim' => '21416255xxxxxx',
+        'kelas' => 'IF21F',
+    ],
+    6 => [
+        'nama' => 'Arief Maulana',
+        'photo' => 'assets/img/photos/Arief.jpg',
+        'nim' => '21416255xxxxxx',
+        'kelas' => 'IF21F',
+    ],
+    7 => [
+        'nama' => 'Ira Sutirahayu',
+        'photo' => 'assets/img/photos/ira s.jpg',
+        'nim' => '21416255xxxxxx',
+        'kelas' => 'IF21F',
+    ],
+    8 => [
+        'nama' => 'Yolanda',
+        'photo' => 'assets/img/photos/4x6.jpg',
+        'nim' => '21416255xxxxxx',
         'kelas' => 'IF21F',
     ],
 
